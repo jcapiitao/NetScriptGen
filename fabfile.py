dff7a36e0585b63fb25450420a48689d86b1ed32
Joel Capitao <joel.capitao93@gmail.com>
2015-11-02 23:14:17 +0100

    Creation of the fable for deploying the distribution on the remote host

---
+++ fabfile.py
@@ -0,0 +1,17 @@
+from fabric.api import *
+
+@hosts('trystram.net')
+def deploy_dist(dist_dir='/home/web/koken/dl/', bundle_name='netscriptgen.tar.gz'):
+    local('tar czvf /tmp/{0} ~/ownCloud/Developpement/NetScriptGen/target/dist/NetScriptGen-0.1.0/'.format(bundle_name))
+    put(local_path="/tmp/{0}".format(bundle_name), remote_path='/tmp')
+    with cd(dist_dir):
+        with settings(warn_only=True):
+            result = run('find {0}{1}'.format(dist_dir, bundle_name))
+            # If the file already exists, we delete it
+            if result.return_code == 0:
+                print('Deleting the old bundle on remote host...')
+                run('rm {0}'.format(bundle_name))
+            print('Moving the new bundle from /tmp to {0}...'.format(dist_dir))
+            run('mv /tmp/{0} .'.format(bundle_name))
+    print('Removing the old bundle on local host.')
+    local("rm /tmp/{0}".format(bundle_name))
